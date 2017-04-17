11496343a9753b476c9f13500535c64171abec63
RandomNPC <bluedoctor7@gmail.com>
2017-04-16 22:23:17 -0700

    Combined projects into one hub

---
+++ Penguin Route/data.js
@@ -0,0 +1,97 @@
+var teleports = [
+  ""
+  ,"Ardougne Lodestone"
+  ,"Al Kharid Lodestone"
+  ,"Ape Atoll Teleport"
+  ,"Bandit Camp Lodestone"
+  ,"Burthrope Lodestone"
+  ,"Canifis Lodestone"
+  ,"Draynor Lodestone"
+  ,"Duel Ring - Castle Wars"
+  ,"Duel Ring - Duel Arena"
+  ,"Duel Ring - Mobilising Armies"
+  ,"Ectophial"
+  ,"Edgeville Lodestone"
+  ,"Edgeville Lodestone/Bank"
+  ,"Falador Lodestone"
+  ,"Fremmnik Province Lodestone"
+  ,"Games Necklace - Barbarian Outpost"
+  ,"Karamja Lodestone"
+  ,"Lumbridge Lodestone"
+  ,"Port Sarim Lodestone"
+  ,"Priffindas Lodestone"
+  ,"Seers Village Lodestone"
+  ,"Tiranwann Lodestone"
+  ,"Varrock Lodestone"
+  ,"Yannile Lodestone"
+  ];
+
+
+var locations = [
+  {"":[""]}
+  ,{"Al Kharid":["","Al Kharid Lodestone", "Duel Ring - Duel Arena"]}
+  ,{"Ape Atoll":["","Ape Atoll Teleport","Gnome Stronghold - Daero"]}
+  ,{"Ardougne Well":["","Ardougne Lodestone","Group Teleport - Fishing Guild"]}
+  ,{"Barbarian Outpost":["","Games Necklace - Barbarian Outpost","Group Teleport - Barbarian Outpost"]}
+  ,{"Brimhaven":["","Karamja Lodestone", "Charter Ship - Brimhaven"]}
+  ,{"Burthorpe":["","Burthope Lodestone"]}
+  ,{"Canifis":["","Canifis Lodestone"]}
+  ,{"Castle Wars":["","Duel Ring - Castle Wars","Yannile Lodestone"]}
+  ,{"Central Karamja":["","Karamja Lodestone","Wicked Hood - Nature Altar"]}
+  ,{"Digsite":["","Varrock Lodestone","Wicked Hood - Earth Altar"]}
+  ,{"Dorgesh-Kaan":["","Lumbridge Lodestone"]}
+  ,{"Dragontooth Isle":["","Ectophial"]}
+  ,{"Draynor Manor":["","Draynor Lodestone"]}
+  ,{"Eagles Peak":["","Eagles Peak Lodestone"]}
+  ,{"East Karamja":["","Wicked Hood - Nature Altar","Karamja Lodestone"]}
+  ,{"Elf Camp":["","Tiranwann Lodestone"]}
+  ,{"Elf Limestone":["","Tiranwann Lodestone"]}
+  ,{"Entrana":["","Wicked Hood - Law Altar","Port Sarim Lodestone"]}
+  ,{"Falador Well":["","Port Sarim Lodestone","Falador Lodestone"]}
+  ,{"Feldip Hills":["","Oo'glog Lodestone","Yannile Lodestone","Duel Ring - Mobilising Armies"]}
+  ,{"Gnome Maze":["","Yannile Lodestone","Duel Ring - Castle Wars"]}
+  ,{"Gnome Stronghold":["","Eagles Peak Lodestone"]}
+  ,{"Ice Mountain":["","Edgeville Lodestone"]}
+  ,{"Jatizso":["","Fremmnik Province Lodestone"]}
+  ,{"Jiggig":["","Duel Ring - Castle Wars"]}
+  ,{"KQ Lair":["","Bandit Camp Lodestone"]}
+  ,{"Keldagrim":["","Ring of Wealth - Varrock GE","Edgeville Lodestone","Spirit Tree - GE"]}
+  ,{"Lava Maze":["","Edgeville Lodestone/Bank/Lever","Group Teleport - Ice Plateau"]}
+  ,{"Lighthouse":["","Group Teleport - Barbarian Outpost","Games Necklace - Barbarian Outpost"]}
+  ,{"Lletya":["", "Elf Crystal - Lletya","Tiranwann Lodestone"]}
+  ,{"Lumbridge":["","Lumbridge Lodestone"]}
+  ,{"Lunar Isle":["","Lunar Isle Lodestone","Group Teleport - Lunar Isle"]}
+  ,{"Mage Training Arena":["","Al Kharid Lodestone","Wicked Hood - Fire Altar","Duel Ring - Duel Arena"]}
+  ,{"McGrubbors Woods":["","Seers Village Lodestone"]}
+  ,{"Miscellania":["","Fremmnik Province Lodestone","Ring of Wealth - Miscellania"]}
+  ,{"Mort Myre":["","Canifis Lodestone"]}
+  ,{"Mos Le'Harmless":["","Bill Teach/Ectophial","Charter Ship - Mos Le'Harmless"]}
+  ,{"Mudskipper Point":["","Port Sarim Lodestone"]}
+  ,{"Musa Point":["","Charter Ship - Musa Point","Port Sarim Lodestone/Seaman"]}
+  ,{"Musa Point Well":["","Charter Ship - Musa Point","Port Sarim Lodestone/Seaman"]}
+  ,{"Nardah":[""]}
+  ,{"Neitiznot":["","Fremmnik Province Lodestone"]}
+  ,{"KBD North (Wilderness)":["","Edgeville Lodestone/Bank/Lever","Group Teleport - Ice Plateau"]}
+  ,{"Northwest Ardougne":["","Ardougne Lodestone","Group Teleport - Fishing Guild"]}
+  ,{"Observatory":["","Duel Ring - Castle Wars","Yannile Lodestone"]}
+  ,{"South of Wilderness Volcano":["","Wilderness Lodestone"]}
+  ,{"Piscatoris":["","Eagles Peak Lodestone"]}
+  ,{"Port Khazard":["","Group Teleport - Port Khazard", "Yannile Lodestone"]}
+  ,{"Port Phasmatys":["","Ectophial"]}
+  ,{"Rellekka":["","Fremmnik Province Lodestone"]}
+  ,{"Rellekka Well":["","Fremmnik Province Lodestone"]}
+  ,{"Rimmington Well":["","Port Sarim Lodestone"]}
+  ,{"KBD South (Wilderness)":["","Edgeville Lodestone/Bank/Lever","Group Teleport - Ice Plateau"]}
+  ,{"Sawmill":["","Varrock Lodestone","Wicked Hood - Earth Altar"]}
+  ,{"Scorpion Pit (Wilderness)":["","Edgeville Lodestone/Bank/Lever","Group Teleport - Ice Plateau"]}
+  ,{"Smokey Well":["","Bandit Camp Lodestone"]}
+  ,{"Sophanem":["","Bandit Camp Lodestone"]}
+  ,{"South Pollinveach":["","Bandit Camp Lodestone"]}
+  ,{"Tyras Catapult":["","Tiranwann Lodestone"]}
+  ,{"Varrock Well":["","Varrock Well"]}
+  ,{"White Wolf Mountain":["","Burthrope Lodestone"]}
+  ,{"Witchaven":["","Ardougne Lodestone"]}
+  ,{"Wizards Tower":["","Draynor Lodestone","Wicked Hood - Teleport"]}
+  ,{"Zanaris":["","Lumbridge Lodestone"]}
+  ,{"Goblin Village":["","Falador Lodestone"]}
+];
