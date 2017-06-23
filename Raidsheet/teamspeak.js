3461f0a111184088283de7f7b461912733010eb9
RandomNPC <bluedoctor7@gmail.com>
2017-06-23 07:31:50 -0700

    Fixed issue with teamspeak display showing multiple values

--- Raidsheet/teamspeak.js
+++ Raidsheet/teamspeak.js
@@ -25,13 +25,13 @@ function UpdateTeamspeakDisplay()
                 '[th]Beastmaster[/th]'+"\n"+
                 '[/tr]'+"\n";
 
-  var yaka_role = "DPS";
-  var bm_role = "DPS";
-
 
   $.each($("#content-home .card"),function(index,value){
     var $list = $(value).find(".list");
     var name = $(value).find(".text").text().replace(/^\s+|\s+$/g,"");
+
+    var yaka_role = "DPS";
+    var bm_role = "DPS";
 
     $.each($list.children(),function(i,v){
       var role_id = $(v).attr("class").split(' ')[3].match(/\d+/g)[0];
