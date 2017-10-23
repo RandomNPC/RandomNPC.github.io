66edab05c655ed04bbc4342cd27f15d4751b5a9f
RandomNPC <bluedoctor7@gmail.com>
2017-10-23 10:03:50 -0700

    Added code to test posting events

--- Raid-Calender/validate.js
+++ Raid-Calender/validate.js
@@ -34,6 +34,7 @@ $(document).ready(function(){
 
     if(status)
     {
+      /*
       gapi.client.calendar.events.list({
        'calendarId': CALENDAR_ID,
        'timeMin': (new Date()).toISOString(),
@@ -47,6 +48,44 @@ $(document).ready(function(){
          $("#test").append('<p>' + value.start.dateTime + '</p>');
        });
      });
+     */
+
+     var event = {
+      'summary': 'Test Event',
+      'location': 'Your Nans House',
+      'description': 'A Test Event. This is nothing',
+      'start': {
+        'dateTime': '2017-10-24T12:00:00-07:00',
+        'timeZone': 'America/Los_Angeles'
+      },
+      'end': {
+        'dateTime': '2017-10-24T19:00:00-07:00',
+        'timeZone': 'America/Los_Angeles'
+      },
+      'recurrence': [
+      //  'RRULE:FREQ=DAILY;COUNT=2'
+      ],
+      'attendees': [
+        //{'email': 'lpage@example.com'},
+        //{'email': 'sbrin@example.com'}
+      ],
+      'reminders': {
+        //'useDefault': false,
+        //'overrides': [
+        //  {'method': 'email', 'minutes': 24 * 60},
+        //  {'method': 'popup', 'minutes': 10}
+        //]
+      }
+    };
+
+  var request = gapi.client.calendar.events.insert({
+    'calendarId': CALENDAR_ID,
+    'resource': event
+  });
+
+  request.execute(function(event) {
+    $("#test").append('<p>' + 'Event created: ' + event.htmlLink + '</p>');
+  });
     }
   }
 });
