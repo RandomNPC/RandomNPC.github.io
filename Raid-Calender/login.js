a01d2a43ffdfefba37bf0b9c6d3fc6b4beeae61e
RandomNPC <bluedoctor7@gmail.com>
2017-11-06 19:02:32 -0800

    Fixed links to privacy policy

--- Raid-Calender/login.js
+++ Raid-Calender/login.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
   $('.landing-page .navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
-    //$(document).scrollTop( $($(this).attr("id")).offset().top );
   });
 });
