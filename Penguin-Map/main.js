72c1f51b918fed58576cb7620637f7f1c629c438
RandomNPC <bluedoctor7@gmail.com>
2018-01-09 14:00:57 -0800

    Added interactable penguin map

---
+++ Penguin-Map/main.js
@@ -0,0 +1,89 @@
+const API_KEY = "AIzaSyDkC-bMPs5LSQF1Wqukx1EheLmGQgO0elk";
+const SPREADSHEET_ID = "1daB2uGhejLaeIbNgYvdAOruu9Tr0bu2uOonPK8cC_Ag";
+const BASE_SITE = "https://sheets.googleapis.com/v4/spreadsheets/";
+
+$(document).ready(function(){
+  let map = L.map('map').setView([0,0],4);
+
+  L.tileLayer(`http://www.runeslayer.com/maps/world/6-25-2013/{z}/{x}/{y}.png`,
+  {
+    tms: true,
+    errorTileUrl: "http://www.maptiler.org/img/none.png",
+    minZoom: 3,
+    maxZoom: 8,
+    continousWorld: false,
+    maxBoundsViscosity: 1.0,
+    noWrap: true,
+  }).addTo(map);
+  let bounds = [[-100,3],[-21,-180]];
+
+  map.setMaxBounds(bounds);
+  map.on('drag', function() {
+  	map.panInsideBounds(bounds, { animate: false });
+  });
+
+  firebase.initializeApp(
+    {
+      apiKey: "AIzaSyB42PW3MwrtNhxuAVUgGYFdDfjiVCqNpJ0",
+      authDomain: "penguin-confirm.firebaseapp.com",
+      databaseURL: "https://penguin-confirm.firebaseio.com",
+      projectId: "penguin-confirm",
+      storageBucket: "penguin-confirm.appspot.com",
+      messagingSenderId: "124831971002"
+    }
+  );
+
+  let markers = L.layerGroup().addTo(map);
+
+  firebase.database().ref('penguins').on('value',
+  data=>{
+    let marker_data = [];
+    markers.clearLayers();
+    data.val().forEach((iter,iter_index)=>{
+      if(iter_index < 11)
+      {
+        if(iter.confirm)
+        {
+          //one entry
+          let confirm = iter.entries[iter.index];
+          marker_data.push({
+            strand: (iter_index < 10) ? (iter_index % 5) + 1 : 6,
+            option: iter.index+1,
+            name: confirm.descriptor,
+            id: confirm.index,
+            coords: [confirm.coordinates.x,confirm.coordinates.y],
+          });
+        }
+        else
+        {
+          //both entries
+          iter.entries.forEach((sel,sel_index)=>{
+            marker_data.push({
+              strand: (iter_index < 10) ? (iter_index % 5) + 1 : 6,
+              option: sel_index+1,
+              name: sel.descriptor,
+              id: sel.index,
+              coords: [sel.coordinates.x,sel.coordinates.y],
+            });
+          });
+        }
+      }
+    });
+
+    marker_data.forEach(p=>{
+      MarkLocation(parseFloat(p.coords[1]),parseFloat(p.coords[0]),p);
+    });
+
+    function MarkLocation(x,y,p){
+        let format = `<p>${p.name}<br/>ID:${p.id}<br/>Strand: ${p.strand}<br/>Option: ${p.option}</p>`;
+
+        let m = L.marker([y,x]).addTo(map).bindPopup(format);
+        markers.addLayer(m);
+    }
+
+  },
+  error=>{
+    console.log("Database couldn't update on Value")
+  });
+
+});
