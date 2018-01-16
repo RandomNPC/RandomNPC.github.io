cb7115aef40970dbf2f91d5ac715b8359a924cc9
RandomNPC <bluedoctor7@gmail.com>
2018-01-16 15:47:43 -0800

    Added Alt feature to zoom into alternate

--- Penguin-Map/main.js
+++ Penguin-Map/main.js
@@ -34,10 +34,10 @@ $(document).ready(function(){
   );
 
   let markers = L.layerGroup().addTo(map);
-
+  let marker_data = [];
   firebase.database().ref('penguins').on('value',
   data=>{
-    let marker_data = [];
+    marker_data = [];
     markers.clearLayers();
     data.val().forEach((iter,iter_index)=>{
       if(iter_index < 11)
@@ -48,34 +48,46 @@ $(document).ready(function(){
           let confirm = iter.entries[iter.index];
           marker_data.push({
             strand: (iter_index < 10) ? (iter_index % 5) + 1 : 6,
-            option: iter.index+1,
+            option: iter.index,
             name: confirm.descriptor,
             id: confirm.index,
             coords: [confirm.coordinates.x,confirm.coordinates.y],
+            complement: null,
+            marker_index: marker_data.length,
           });
         }
         else
         {
           //both entries
+          let marker_length = 0;
           iter.entries.forEach((sel,sel_index)=>{
-            marker_data.push({
+            marker_length = marker_data.push({
               strand: (iter_index < 10) ? (iter_index % 5) + 1 : 6,
-              option: sel_index+1,
+              option: sel_index,
               name: sel.descriptor,
               id: sel.index,
               coords: [sel.coordinates.x,sel.coordinates.y],
+              complement: null,
+              marker_index: marker_data.length,
             });
           });
+          //last two indexes
+          marker_data[marker_length-1].complement = marker_data[marker_length-2];
+          marker_data[marker_length-2].complement = marker_data[marker_length-1];
         }
       }
     });
-
     marker_data.forEach(p=>{
       MarkLocation(parseFloat(p.coords[1]),parseFloat(p.coords[0]),p);
     });
 
     function MarkLocation(x,y,p){
-        let format = `<p>${p.name}<br/>ID:${p.id}<br/>Strand: ${p.strand}<br/>Option: ${p.option}</p>`;
+        let format = `<h4>${p.name}</h4>`;
+        if(p.complement != null)
+        {
+          format+=`Alternate: <a id="${p.complement.marker_index}">${p.complement.name}</a>`;
+        }
+
         let disguise_map =
         {
           "rock": {
@@ -106,8 +118,6 @@ $(document).ready(function(){
 
         let icon_disguise = disguise_map[`${p.name.match(/(\w+)$/g)[0].toLowerCase()}`];
 
-        console.log("x"+p.name.match(/(\w+)$/g)[0].toLowerCase()+"x")
-
         let custom_icon = L.icon({
           iconUrl: icon_disguise.url,
           iconSize: icon_disguise.size,
@@ -121,4 +131,8 @@ $(document).ready(function(){
     console.log("Database couldn't update on Value")
   });
 
+  $("body").on('click','a',function(){
+    let alt_index = $(this)[0].id;
+    map.panTo([marker_data[alt_index].coords[0],marker_data[alt_index].coords[1]]);
+  })
 });
