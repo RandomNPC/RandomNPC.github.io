5ca5abdda86f89569fcbeecb7421a2e8556bad79
RandomNPC <bluedoctor7@gmail.com>
2018-03-13 17:00:39 -0700

    Made edit

--- Penguin-Confirm/navigation.js
+++ Penguin-Confirm/navigation.js
@@ -2,19 +2,19 @@ $(document).ready(()=>{
   $("#content-setup, #content-location").toggle(false);
 
   $("#link-home").click(()=>{
-    $("#content-setup, #content-home, #content-locations").toggle(false);
+    $("#content-setup, #content-home, #content-location").toggle(false);
     $("#content-home").toggle();
     $(".navbar-collapse").collapse("hide");
   });
 
   $("#link-setup").click(()=>{
-    $("#content-setup, #content-home, #content-locations").toggle(false);
+    $("#content-setup, #content-home, #content-location").toggle(false);
     $("#content-setup").toggle();
     $(".navbar-collapse").collapse("hide");
   });
 
   $("#link-locations").click(()=>{
-    $("#content-setup, #content-home, #content-locations").toggle(false);
+    $("#content-setup, #content-home, #content-location").toggle(false);
     $("#content-location").toggle();
     $(".navbar-collapse").collapse("hide");
   });
