95b05baa4a3c26900123dde7533e6475386df72a
RandomNPC <bluedoctor7@gmail.com>
2018-07-28 16:08:47 -0700

    Rewrote the raidsheet for optomization

--- Raidsheet/database.js
+++ Raidsheet/database.js
@@ -12,129 +12,117 @@ $(document).ready(function(){
   };
   firebase.initializeApp(config);
 
-  var db_ref = firebase.database().ref(DATABASE);
-  db_ref.on('value',Update,(error)=>{});
+  const arr =
+  $(`#content-home .card`).map((x,i)=>[[`.text.padding`,`.form-control`,`.list`,`.card-block button`].map(k=>$(i).find(k))])
 
-  $("#button-reset").click(function(){
-    if(confirm("Wipe raid data?"))
-    {
-      ResetDatabase();
-      UpdateTeamspeakDisplay();
-      UpdateVideoDisplay();
-    }
-  });
+  firebase.database()
+          .ref(`players`)
+          .once(`value`)
+          .then(data=>{
+            data.val().forEach((x,i)=>{
+              $(arr[i][0]).text(x.name);
+              //$(arr[i][1]).val(x.name);
+              x.roles.forEach(k=>{
+                if(k>=0){
+                  let $ref = $($(arr[i][3])[k]);
 
-  $.ajax({
-      type: 'GET',
-      url: 'https://us-central1-raid-calender.cloudfunctions.net/getEvents',
-      contentType: 'application/json',
-      xhrFields: {
-        withCredentials: false
-      },
-      success: function(data){
-        $.map(data,(value)=>{
-          $("#events").append('<a class="dropdown-item '+value.startTime+'" href="#">'+moment(value.startTime).format('dddd, MMMM DD hh:mm a')+'</a>');
-        });
-      }
-  });
+                  //Copy this role to the role list for said person
+                  $ref.clone().appendTo($(arr[i][2]))
 
-  $("#events").on("click",".dropdown-item",function(){
-    let target = $(this).attr('class').split(' ');
-    if(target.length > 1){
-      //Not Default
-      $.ajax({
-          type: 'GET',
-          url: 'https://us-central1-raid-calender.cloudfunctions.net/getEvents',
-          contentType: 'application/json',
-          xhrFields: {
-            withCredentials: false
-          },
-          success: function(data){
-            $.map(data,(value)=>{
-              if(target[1] == value.startTime){
-                var db = firebase.database().ref(DATABASE+"/0");
+                  //Hide other copies of this role
+                  arr.map((p,q)=>q[3][k])
+                     .toArray()
+                     .filter((s,t)=>t!=i)
+                     .forEach(u=>$(u).hide())
+                }
+              })
+            })
+          })
 
-                db.update({
-                  "name": value.host,
-                });
+  firebase.database()
+          .ref(`players`)
+          .on(`child_changed`,(data)=>{
 
-                $.each(value.attendees,(index,player)=>{
-                  firebase.database().ref(DATABASE+"/"+(index+1)).update({
-                    "name": player,
-                  });
-                });
+            let target = arr[data.key];
+            let content = data.val();
+
+            $(target[0]).text(content.name);
+            //$(target[1]).val(content.name);
+            $(target[2]).find(`button:not(.btn-warning)`).remove();
+
+            content.roles.forEach(k=>{
+              let $ref = $($(target[3])[k]);
+              $ref.clone().appendTo($(target[2]))
+            })
+
+            //show all buttons
+            $(arr.map((x,i)=>i[3].toArray())).show();
+
+            firebase.database()
+                    .ref(`players`)
+                    .once(`value`)
+                    .then(arg=>{
+                      //What roles have been assigned
+                      let active_roles =
+                      arg.val()
+                         .map(x=>x.roles);
+
+                      //Collect each role into an array
+                      let selected_roles =
+                      active_roles.reduce((x,i)=>x.concat(i))
+                                  .filter(x=>x>=0)
+
+                      active_roles.map(x=>selected_roles.filter(k=>!x.includes(k)))
+                                  .forEach((x,i)=>x.forEach(k=>$(arr[i][3][k]).hide()))
+
+                    })
+          })
+
+  $($(arr.map((x,i)=>i[3].toArray()))).click(function(){
+    //Which button was pressed?
+    let $target = $(this)
+
+    //Which card index was this clicked?
+    let target_index = $(`#content-home .card`).toArray().indexOf($target.parents(`.card`)[0]);
+
+    //Which index was pressed?
+    let role_index = $(arr[target_index][3]).toArray().indexOf($target[0])
+
+    //Get the person's current role indicies and update them
+    firebase.database()
+            .ref(`players/${target_index}/roles`)
+            .once(`value`)
+            .then(x=>{
+              if(x.val().includes(role_index))
+              {
+                let roles = x.val().filter(k=>k!=role_index)
+                return (roles.length > 0) ? roles : [-1];
               }
-            });
-          }
-      });
-    }
-    else {
-      //Default
-      ResetDatabase();
-    }
-  });
+              else {
+                return x.val().concat(role_index).filter(k=>k>=0)
+              }
+            })
+            .then(x=>firebase.database().ref(`players/${target_index}/roles`).set(x))
+            .catch(err=>console.log(err))
+  })
+
+  $($(arr.map((x,i)=>i[1].toArray()))).change(function(){
+    //Which textbox was changed?
+    let $target = $(this)
+
+    //Which card index was changed?
+    let target_index = $(`#content-home .card`).toArray().indexOf($target.parents(`.card`)[0]);
+
+    //Update Database
+    firebase.database()
+            .ref(`players/${target_index}`)
+            .update({name:$target.val()})
+            .catch(err=>console.log(err))
+  })
+
+  $(`#button-reset`).click(function(){
+    firebase.database()
+            .ref(`players`)
+            .set(Array(10).fill(null).map((x,i)=>{return {name:`Player ${i+1}`,roles:[-1]}}));
+  })
 });
-
-function Update(data)
-{
-  //Purge all lists
-  $.map($(".list"),(v)=>{$(v).empty();});
-  $.map($(".card-block :hidden"),(v)=>{$(v).show();});
-
-  $("#content-home .list:eq(0)").append("<button type=\"button\" class=\"btn btn-warning btn-space\">Host</button>");
-
-  $.each(data.val(),(index,value)=>{
-
-    var $card = $($("#content-home .card")[index]);
-    var $list = $card.find(".list");
-    var $roles = $card.find(".card-block .btn");
-
-    var $text = $card.find(".text");
-    var $form = $card.find(".form-control");
-
-    //Get Roles
-    var extracted_roles = value.roles.toString().match(/[^,]+/g).sort(function(a,b){
-      return parseInt(a) - parseInt(b);
-    });
-
-    //Append Roles
-    $.map(extracted_roles,(v)=>{
-      if(v >= 0)
-      {
-        var $ref = $($roles[v]);
-        $ref.clone().appendTo($list);
-      }
-    })
-
-    //Set Text
-    //$form.val(value.name);
-    $text.text(value.name);
-
-    //Turn off roles
-    $.each($("#content-home .card").not($card), (index,value)=>{
-      $.each(extracted_roles, (i,v)=>{$($(value).find(".card-block .btn")[v]).hide();});
-    });
-  });
-
-  //Update Teamspeak
-  UpdateTeamspeakDisplay();
-
-  //Update Video
-  UpdateVideoDisplay();
-}
-
-function ResetDatabase()
-{
-  var db_ref = firebase.database().ref(DATABASE);
-  var db_contents = [];
-
-  $.each($("#content-home .card"),function(index,value){
-    var player_data =
-    {
-      name: "Player "+index,
-      roles: "-1",
-    };
-    db_contents.push(player_data);
-  });
-  db_ref.update(db_contents);
-}
